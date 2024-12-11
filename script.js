8d119f48e161bd7492bf334a41cec0f22cc5486b
vliotta <vin.liotta@free.fr>
2024-12-11 17:03:17 +0100

    v1.6 wip API connection

---
+++ script.js
@@ -0,0 +1,10 @@
+const apiKey = '6572fbd15c185aa01ffa6d2147c90170';
+const apiUrl = 'https://api.example.com/data';
+
+const form = document.querySelector('.search');
+const searchInput = document.getElementById('searchSaisi');
+const icon = document.getElementById('icone');
+const tempElement = document.querySelector('.temp');
+const humidityElement = document.querySelector('.humidity');
+const windSpeedElement = document.querySelector('.wind_speed');
+
