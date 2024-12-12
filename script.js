4fe669e3db6915b95b8966a39a71728934d2110e
vliotta <vin.liotta@free.fr>
2024-12-12 17:22:19 +0100

    v1.7 end animation - wip API

--- script.js
+++ script.js
@@ -1,5 +1,4 @@
 const apiKey = '6572fbd15c185aa01ffa6d2147c90170';
-const apiUrl = 'https://api.example.com/data';
 
 const form = document.querySelector('.search');
 const searchInput = document.getElementById('searchSaisi');
@@ -8,3 +7,35 @@ const tempElement = document.querySelector('.temp');
 const humidityElement = document.querySelector('.humidity');
 const windSpeedElement = document.querySelector('.wind_speed');
 
+form.addEventListener('submit', (e) => {
+    e.preventDefault();
+if (searchInput.value) {
+    fetch('https://api.openweathermap.org/data/2.5/weather?q=${searchInput.value}&APPID=${apiKey}')
+    .then(response => {
+      if (!response.ok) {
+        throw new Error('Network response was not ok');
+      }
+      return response.json();
+    })
+    .then(data => {
+      humidityElement.innerHTML = data.main.humidity;
+      windSpeedElement.innerHTML = Math.round(data.wind.speed);
+      tempElement.innerHTML = Math.round(data.main.temp);
+      if (data.weather.length) {
+        icon.src = '';
+      } else {
+        icon.src = '';
+      }
+    })
+    .catch(() => {
+        humidityElement.innerText = '-';
+        windSpeedElement.innerText = '-';
+        tempElement.innerText = '-';
+    });
+}
+else {
+    alert('Veuillez entrer une ville');
+    return;
+}
+    
+  });
