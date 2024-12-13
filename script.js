49e7ed5b119ae611f127f4eb256d19bfd70aae96
vliotta <vin.liotta@free.fr>
2024-12-13 16:05:57 +0100

    v1.9 wip script.js

--- script.js
+++ script.js
@@ -10,7 +10,7 @@ const windSpeedElement = document.querySelector('.wind_speed');
 form.addEventListener('submit', (e) => {
     e.preventDefault();
 if (searchInput.value) {
-    fetch('https://api.openweathermap.org/data/2.5/weather?q=${searchSaisi.value}&APPID=${apiKey}')
+    fetch(`https://api.openweathermap.org/data/2.5/weather?q=${searchInput.value}&APPID=${apiKey}`)
     .then(response => {
       if (!response.ok) {
         throw new Error('Network response was not ok');
@@ -22,7 +22,8 @@ if (searchInput.value) {
       humidityElement.innerText = data.main.humidity;
       windSpeedElement.innerText = Math.round(data.wind.speed);
       if (data.weather.length) {
-        icon.src = '';
+        icon.src = `https://openweathermap.org/img/wn/${data.weather[0].icon}@2x.png`;
+        icon.style.display = 'block';
       } else {
         icon.src = '';
       }
